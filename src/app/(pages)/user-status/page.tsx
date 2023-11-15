15e9f9f16bdaf2492624369664f823fa35c8cfac
Rocky Wang <wangrocky89@gmail.com>
2023-11-15 20:48:25 +1100

    realtime online status

--- src/app/(pages)/user-status/page.tsx
+++ src/app/(pages)/user-status/page.tsx
@@ -1,70 +1,38 @@
 'use client'
 import { database } from "@/app/firebase"
-import { ref, onDisconnect, onValue, query, limitToLast, get } from "firebase/database"
+import { FirebaseApp } from "firebase/app"
+import { ref, onDisconnect, onValue, query, limitToLast, get, update, off } from "firebase/database"
 import { useEffect, useRef, useState } from "react"
 import './style.css'
 
 export default function Page() {
-    const connectedRef = ref(database, '.info/connected')
-    const msgRef = query(ref(database, 'u01/messages'), limitToLast(4))
-    const [msgList, setMsgList] = useState([])
-    const currentPage = useRef(1)
+    const statusRef = ref(database, 'u01/status')
+    const statusListener = useRef(null)
+    // const msgRef = query(ref(database, 'u01/messages'), limitToLast(4))
 
     useEffect(()=>{
-        onValue(connectedRef, (snapshot) => {
-            const snapshotValue = snapshot.val()
-            console.log(snapshotValue)
-            if(snapshotValue === true) {
-                console.log("connected")
-            }
-            else {
-                console.log("not connected")
-            }
+        console.log('add listener')
+        onValue(statusRef, (snapshot)=>{
+            console.log(snapshot.val())
+            update(statusRef, {online: true})
         })
+        onDisconnect(statusRef).update({online: false})
+        return () => {
+            off(statusRef, 'value')
+            update(statusRef, {online: false})
+        }
     }, [])
 
-    useEffect(()=>{
-        // onValue(msgRef, (snapshot) => {
-        //     const newMessagesObj = snapshot.val()
-        //     const newMessageList = []
-        //     for(let key in newMessagesObj) {
-        //         newMessageList.unshift(newMessagesObj[key])
-        //     }
-        //     setMsgList(newMessageList)
-        // })
-        onValue(msgRef, (snapshot) => {
-            get(query(ref(database, 'u01/messages'), limitToLast(currentPage.current * 4)))
-            .then(res => {
-                const newMessagesObj = res.val()
-                console.log(newMessagesObj)
-                const newMessageList = []
-                for(let key in newMessagesObj) {
-                    newMessageList.unshift(newMessagesObj[key])
-                }
-                setMsgList(newMessageList)
-            })
-        })
-    }, [])
-
-    function loadMore() {
-        get(query(ref(database, 'u01/messages'), limitToLast(++currentPage.current * 4)))
-        .then(res => {
-            const newMessagesObj = res.val()
-            console.log(newMessagesObj)
-            const newMessageList = []
-            for(let key in newMessagesObj) {
-                newMessageList.unshift(newMessagesObj[key])
-            }
-            setMsgList(newMessageList)
+    const handleConnect = () => {
+        onValue(statusRef, (snapshot)=>{
+            console.log(snapshot.val())
+            update(statusRef, {online: true})
         })
     }
 
     return <>
         <h1>user status page</h1>
-        <button>Disconnect DB</button>
-        {
-            msgList.map(item => <div key={item.id} className="message-card">{item.text}</div>)
-        }
-        <button onClick={loadMore}>Load more</button>
+        {/* <button onClick={handleDisconnect}>Disconnect DB</button> */}
+        <button onClick={handleConnect}>Connect DB</button>
     </>
 }
